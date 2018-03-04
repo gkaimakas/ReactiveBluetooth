6de027482b3279bbdd302659d6393c333b0468e3
gkaimakas <gkaimakas@gmail.com>
2018-03-04 21:38:16 +0200

    Initial commit

---
+++ Example/ReactiveBluetooth/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ReactiveBluetooth
+//
+//  Created by gkaimakas on 03/04/2018.
+//  Copyright (c) 2018 gkaimakas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
